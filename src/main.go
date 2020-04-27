a4990a48c350a50e19c9275ca92fae8b079ec378
ltt <280635302@qq.com>
2020-04-27 18:38:22 +0800

    修改

--- src/main.go
+++ src/main.go
@@ -16,11 +16,15 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	t := time.Now()
+	tm1 := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	fmt.Println(tm1.Unix())
+
 	// 创建must组件实例
 	must.Init()
 	mustComponent()
 
-	common.LogDeBug("run start")
+	fmt.Println("run start")
 	// 持久化
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan,
