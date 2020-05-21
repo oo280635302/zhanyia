55446940296120d0595d34e3e7a70b86e3d1c67c
zy <280635302@qq.com>
2020-05-21 22:40:40 +0800

    offer note

--- src/main.go
+++ src/main.go
@@ -22,6 +22,11 @@ func main() {
 	must.Init()
 	mustComponent()
 
+	//
+	a := &pMergedAB{}
+	var length *LenMerged
+	MergedStructAB1([]*ProA{{'a', 'a', 1}, {'c', 'a', 3}}, 1, []*ProB{{'a', 'a', 1}}, 1, a, length)
+
 	fmt.Println("run start")
 	// 持久化
 	signalChan := make(chan os.Signal, 1)
@@ -74,4 +79,73 @@ func mapSpace() {
 	}
 	n := common.ImageToSqArray(a)
 	common.PrintDoubleMap(n)
+}
+
+// 精灵云 笔试第一题 合并结构体
+type ProA struct {
+	key1 byte
+	key2 byte
+	val1 int
+}
+
+type ProB struct {
+	key1 byte
+	key2 byte
+	val2 int
+}
+
+type MergedAB struct {
+	key1 byte
+	key2 byte
+	val1 int
+	val2 int
+}
+
+type pMergedAB struct {
+	val  *MergedAB
+	next *pMergedAB
+}
+
+type LenMerged int
+
+func (p *pMergedAB) insert(pMergedAB *pMergedAB) {
+
+	temp := p
+	for {
+		if temp.next == nil {
+			break
+		}
+		temp = temp.next
+	}
+	if temp.val == nil {
+		temp.val = pMergedAB.val
+	} else {
+		temp.next = pMergedAB
+	}
+}
+
+// method 1
+func MergedStructAB1(arrA []*ProA, lenA int, arrB []*ProB, lenB int, mergedAB *pMergedAB, lengthMergedAB *LenMerged) {
+
+	for _, v1 := range arrA {
+		for _, v2 := range arrB {
+			if v1.key1 == v2.key1 && v1.key2 == v2.key2 {
+				mergedAB.insert(&pMergedAB{val: &MergedAB{key1: v1.key1, key2: v2.key2, val1: v1.val1, val2: v2.val2}})
+			}
+		}
+	}
+	fmt.Println(mergedAB)
+}
+
+// method 2
+func MergedStructAB2(arrA []*ProA, lenA int, arrB []*ProB, lenB int, mergedAB *pMergedAB, lengthMergedAB *LenMerged) {
+
+	for _, v1 := range arrA {
+		for _, v2 := range arrB {
+			if v1.key1 == v2.key1 && v1.key2 == v2.key2 {
+				mergedAB.insert(&pMergedAB{val: &MergedAB{key1: v1.key1, key2: v2.key2, val1: v1.val1, val2: v2.val2}})
+			}
+		}
+	}
+	fmt.Println(mergedAB)
 }
