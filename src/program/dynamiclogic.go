59930b31e998dcb91cde580d96734cb01002ac12
zy <280635302@qq.com>
2020-07-02 00:01:38 +0800

    dynamic sliding window note ++

--- src/program/dynamiclogic.go
+++ src/program/dynamiclogic.go
@@ -24,3 +24,58 @@ func WordBreak(s string, wordDict []string) bool {
 	}
 	return dp[len(s)]
 }
+
+// 最长重复子数组-----------------------------------------------------------------------------------------------
+// 思路：滑动窗口 以A、B各为固定点 移动B、A 每次移动都 遍历A B求出其当前最长重复子数组 然后汇总 32ms/3.4mb
+// 多解题思路：动态规划（弱）  二分hash法（强）
+func findLength(A []int, B []int) int {
+	n, m := len(A), len(B)
+	ret := 0
+	for i := 0; i < n; i++ {
+		lenA := min(m, n-i)
+		maxLen := maxLength(A, B, i, 0, lenA)
+		ret = max(ret, maxLen)
+	}
+	for i := 0; i < m; i++ {
+		lenB := min(n, m-i)
+		maxLen := maxLength(A, B, 0, i, lenB)
+		ret = max(ret, maxLen)
+	}
+	return ret
+}
+
+func maxLength(A, B []int, addA, addB, len int) int {
+	ret, k := 0, 0
+	for i := 0; i < len; i++ {
+		if A[addA+i] == B[addB+i] {
+			k++
+		} else {
+			k = 0
+		}
+		ret = max(ret, k)
+	}
+	return ret
+}
+
+// 动态规划解法: 基于暴力解法之上，保存 i,j 位置 当i,j相等时 他们的公共长度 是i+1,j+1的公共长度+1 否则他们公共长度为0
+func findLength2(A []int, B []int) int {
+	n, m := len(A), len(B)
+	dp := make([][]int, n+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, m+1)
+	}
+	ans := 0
+	for i := n - 1; i >= 0; i-- {
+		for j := m - 1; j >= 0; j-- {
+			if A[i] == B[j] {
+				dp[i][j] = dp[i+1][j+1] + 1
+			} else {
+				dp[i][j] = 0
+			}
+			if ans < dp[i][j] {
+				ans = dp[i][j]
+			}
+		}
+	}
+	return ans
+}
