a87727aaea48a7f2b4f147a997f2b91f26ea74f6
战衣丶 <280635302qq.com>
2020-07-19 19:34:27 +0800

    stack Temperature

--- src/program/dynamiclogic.go
+++ src/program/dynamiclogic.go
@@ -508,3 +508,28 @@ func IsInterleave(s1 string, s2 string, s3 string) bool {
 
 	return f[n][m]
 }
+
+// 戳气球-------------------------------------------------------------------------------------------------------------
+// 思路：
+func maxCoins(nums []int) int {
+	n := len(nums)
+	rec := make([][]int, n+2)
+	for i := 0; i < n+2; i++ {
+		rec[i] = make([]int, n+2)
+	}
+	val := make([]int, n+2)
+	val[0], val[n+1] = 1, 1
+	for i := 1; i <= n; i++ {
+		val[i] = nums[i-1]
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 2; j <= n+1; j++ {
+			for k := i + 1; k < j; k++ {
+				sum := val[i] * val[k] * val[j]
+				sum += rec[i][k] + rec[k][j]
+				rec[i][j] = max(rec[i][j], sum)
+			}
+		}
+	}
+	return rec[0][n+1]
+}
