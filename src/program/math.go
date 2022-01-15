2adbeeb3699f79e9cbc84034407293103bc4362f
zhanyia <280635302@qq.com>
2022-01-15 23:28:56 +0800

    leet-code every-question

--- src/program/math.go
+++ src/program/math.go
@@ -7,3 +7,13 @@ import "math"
 func myPow(x float64, n int) float64 {
 	return math.Pow(x, float64(n))
 }
+
+// 计算力扣银行的钱
+// 思路：底为28元，每周比上周多7元 + 每天比昨天多1元 + 周 * 天
+func totalMoney(n int) int {
+	x := n / 7
+	y := n % 7
+
+	res := x*21 + (1+x)*x/2*7 + (y * x) + ((1 + y) * y / 2)
+	return res
+}
--- src/program/test.go
+++ src/program/test.go
@@ -3,6 +3,6 @@ package program
 import "fmt"
 
 func Ingress() {
-	a := kSmallestPairs([]int{1, 3, 5, 7, 9, 11, 13}, []int{2, 4, 6, 8, 10, 12}, 3)
+	a := totalMoney(9)
 	fmt.Println(a)
 }
