2e6ec4085552858fceff41f3ddc356490efee6bd
zhanyia <280635302@qq.com>
2022-03-01 09:03:44 +0800

    最多换楼

--- src/program/backtracking.go
+++ src/program/backtracking.go
@@ -88,3 +88,49 @@ func dfsRestoreIpAddresses(s string, segId, segStart int) {
 		}
 	}
 }
+
+// 最多可达成的换楼请求数目
+// 思路：回缩+枚举
+func maximumRequests(n int, requests [][]int) (ans int) {
+	delta := make([]int, n)
+	cnt, zero := 0, n
+	var dfs func(int)
+	dfs = func(pos int) {
+		if pos == len(requests) {
+			if zero == n && cnt > ans {
+				ans = cnt
+			}
+			return
+		}
+
+		// 不选 requests[pos]
+		dfs(pos + 1)
+
+		// 选 requests[pos]
+		z := zero
+		cnt++
+		r := requests[pos]
+		x, y := r[0], r[1]
+		if delta[x] == 0 {
+			zero--
+		}
+		delta[x]--
+		if delta[x] == 0 {
+			zero++
+		}
+		if delta[y] == 0 {
+			zero--
+		}
+		delta[y]++
+		if delta[y] == 0 {
+			zero++
+		}
+		dfs(pos + 1)
+		delta[y]--
+		delta[x]++
+		cnt--
+		zero = z
+	}
+	dfs(0)
+	return
+}
