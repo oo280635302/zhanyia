6bb928cbf496dc74e821e8ff0ab00b0fa46c57a8
zhanyia <luo.tianwen@cloudcade.com>
2023-03-28 17:06:23 +0800

    dp note ++

--- src/program/dynamic.go
+++ src/program/dynamic.go
@@ -1035,3 +1035,25 @@ func maximalSquare(matrix [][]byte) int {
 
 	return maxside * maxside
 }
+
+// 最长公共子序列
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for idx := range dp {
+		dp[idx] = make([]int, n+1)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			f := text1[i] == text2[j]
+			if f {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+			}
+		}
+	}
+
+	return dp[m][n]
+}
